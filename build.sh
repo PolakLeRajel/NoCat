753b8c7b978d9b3377ce04224de90ce6af133463
Florian Roth <florian.roth@nextron-systems.com>
2025-11-27 13:12:51 +0100

    chore: reworked build script - smaller binaries

--- build.sh
+++ build.sh
@@ -3,15 +3,15 @@
 set -e
 
 # Windows
-GOOS=windows GOARCH=amd64 go build -o nocat_windows_amd64.exe nocat.go
-GOOS=windows GOARCH=arm64 go build -o nocat_windows_arm64.exe nocat.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o nocat_windows_amd64.exe nocat.go
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -ldflags="-s -w" -o nocat_windows_arm64.exe nocat.go
 
 # macOS
-GOOS=darwin GOARCH=amd64 go build -o nocat_darwin_amd64 nocat.go
-GOOS=darwin GOARCH=arm64 go build -o nocat_darwin_arm64 nocat.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o nocat_darwin_amd64 nocat.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o nocat_darwin_arm64 nocat.go
 
 # Linux
-GOOS=linux GOARCH=amd64 go build -o nocat_linux_amd64 nocat.go
-GOOS=linux GOARCH=arm64 go build -o nocat_linux_arm64 nocat.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o nocat_linux_amd64 nocat.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o nocat_linux_arm64 nocat.go
 
 echo "Build completed."
